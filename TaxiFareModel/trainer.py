82b1fc802772157f70ae657ff4e2b374fba9eb32
Yassin Gofti <gofti.yassin@gmail.com>
2021-11-16 14:41:23 +0100

    MLFlow Push

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -8,7 +8,9 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from TaxiFareModel.utils import compute_rmse
-
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
 
 
 class Trainer():
@@ -20,6 +22,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = "[FR] [Paris] [yassingofti] taxifare v1"
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -53,6 +56,27 @@ class Trainer():
         rmse = compute_rmse(y_pred, y_test)
         print(rmse)
         return rmse
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri("https://mlflow.lewagon.co/")
+        return MlflowClient()
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(
+                self.experiment_name).experiment_id
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
 
 
 if __name__ == "__main__":
@@ -67,3 +91,14 @@ if __name__ == "__main__":
     trainer.set_pipeline()
     trainer.run()
     print(trainer.evaluate(X_test, y_test))
+    yourname = "yassingofti"
+
+    if yourname is None:
+        print("please define your name, il will be used as a parameter to log")
+
+    for model in ["linear", "Randomforest"]:
+        client = trainer.mlflow_client
+        run = client.create_run(trainer.mlflow_experiment_id)
+        client.log_metric(run.info.run_id, "rmse", 4.5)
+        client.log_param(run.info.run_id, "model", model)
+        client.log_param(run.info.run_id, "student_name", yourname)
